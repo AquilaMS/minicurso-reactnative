c1d4914be69dbe92747234d7a3b75098160913a8
Diego Fernandes <diego.schell.f@gmail.com>
2017-11-14 21:57:14 -0200

    Estilo do repo, state e props

--- App.js
+++ App.js
@@ -6,24 +6,41 @@ import {
   StyleSheet,
   ScrollView,
   Platform,
+  TouchableOpacity,
 } from 'react-native';
 
-export default class App extends Component<{}> {
+import Repo from './components/Repo';
+
+export default class App extends Component {
+  state = {
+    repos: [
+      {
+        id: 1,
+        thumbnail: 'https://avatars0.githubusercontent.com/u/28929274?s=200&v=4',
+        title: 'rocketseat.com.br',
+        author: 'rocketseat',
+      },
+      {
+        id: 2,
+        thumbnail: 'https://avatars0.githubusercontent.com/u/28929274?s=200&v=4',
+        title: 'rocketseat.com.br',
+        author: 'diego3g',
+      },
+    ],
+  };
+
   render() {
     return (
       <View style={styles.container}>
         <View style={styles.header}>
           <Text style={styles.headerText}>Minicurso GoNative</Text>
+          <TouchableOpacity onPress={() => {}}>
+            <Text style={styles.headerButton}>+</Text>
+          </TouchableOpacity>
         </View>
 
         <ScrollView contentContainerStyle={styles.repoList}>
-          <View style={styles.repo} />
-          <View style={styles.repo} />
-          <View style={styles.repo} />
-          <View style={styles.repo} />
-          <View style={styles.repo} />
-          <View style={styles.repo} />
-          <View style={styles.repo} />
+          { this.state.repos.map(repo => <Repo key={repo.id} data={repo} />) }
         </ScrollView>
       </View>
     );
@@ -39,8 +56,10 @@ const styles = StyleSheet.create({
   header: {
     height: (Platform.OS === 'ios') ? 70 : 50,
     paddingTop: (Platform.OS === 'ios') ? 20 : 0,
+    paddingHorizontal: 20,
     backgroundColor: '#FFF',
-    justifyContent: 'center',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
   },
 
@@ -49,15 +68,12 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
   },
 
-  repoList: {
-    padding: 20,
+  headerButton: {
+    fontSize: 24,
+    fontWeight: 'bold',
   },
 
-  repo: {
+  repoList: {
     padding: 20,
-    backgroundColor: '#FFF',
-    height: 120,
-    marginBottom: 20,
-    borderRadius: 5,
   },
 });
